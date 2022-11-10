404aa3569c4130d4b3426e3fb2be02ecb1e5d386
DopeyGreg <szulik.grzegorz@gmail.com>
2022-11-10 23:50:32 +0100

    uploadform i hash password w rejestracji

---
+++ uploadform.php
@@ -0,0 +1,23 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select Image File to Upload:
+    <input type="file" name="file">
+
+    <select name="category">
+      <option value=NULL selected></option>
+      <option value="po">Poziome</option>
+      <option value="pi">Pionowe</option>
+      <option value="w">Wstawki</option>
+      <option value="r">Ramki</option>
+      <option value="n">Narożne</option>
+      <option value="z">Zawieszki</option>
+    </select>
+
+    <select name="subcategory">
+      <option value="" selected></option>
+      <option value="b">Boże narodzenie</option>
+      <option value="wi">Wielkanoc</option>
+      <option value="j">Jesień</option>
+    </select>
+
+    <input type="submit" name="submit" value="Upload">
+</form>
