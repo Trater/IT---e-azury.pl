0ed75647f6c9bedf19e0509e3f6f57c7ca5bdd3e
DopeyGreg <szulik.grzegorz@gmail.com>
2022-11-11 13:16:40 +0100

    logowanie z hashowanym haslem

--- login.php
+++ login.php
@@ -18,32 +18,49 @@ else
 
 
 
-if($result = @$polaczenie->query(
-    sprintf("SELECT * FROM users WHERE user_email = '%s' AND user_password = '%s'",
-    mysqli_real_escape_string($polaczenie,$email),
-    mysqli_real_escape_string($polaczenie,$password))))
-{
-    $number_of_users = $result->num_rows;
-    if($number_of_users>0)
+//if($result = @$polaczenie->query(
+  //  sprintf("SELECT * FROM users WHERE user_email = '%s' AND user_password = '%s'",
+    //mysqli_real_escape_string($polaczenie,$email),
+    //mysqli_real_escape_string($polaczenie,$password))))
+    if($result = @$polaczenie->query(sprintf("SELECT * FROM users WHERE user_email = '%s'",
+    mysqli_real_escape_string($polaczenie,$email))))
     {
-        $_SESSION['zalogowany'] = true;
-        $column = $result->fetch_assoc();
-        $_SESSION['user_id'] = $column['user_id'];
-        $_SESSION['user_name'] = $column['user_name'];
-        $_SESSION['user_email'] = $column['user_email'];
-        $_SESSION['user_role'] = $column['user_role'];
+        $column=$result->fetch_assoc();
+        $hashed_password = $column['user_password'];
+        if(password_verify(mysqli_real_escape_string($polaczenie,$password),$hashed_password))
+        {
+            $number_of_users = $result->num_rows;
+            if($number_of_users>0)
+            {
+                $_SESSION['zalogowany'] = true;
+                $column = $result->fetch_assoc();
+                $_SESSION['user_id'] = $column['user_id'];
+                $_SESSION['user_name'] = $column['user_name'];
+                $_SESSION['user_email'] = $column['user_email'];
+                $_SESSION['user_role'] = $column['user_role'];
 
-        $result->close();
-       
-        header('Location: index.php');
-    } else {
+                $result->close();
+            
+                header('Location: index.php');
+            }
+        }
+        else 
+        {
+            echo "<script>
+            alert('Niepoprawne dane. Spróbuj ponownie!');
+            window.location.href='index.php';
+            </script>";
+            exit();
+        }
+    }
+    else 
+    {
         echo "<script>
         alert('Niepoprawne dane. Spróbuj ponownie!');
         window.location.href='index.php';
         </script>";
         exit();
     }
-}
 
 $polaczenie->close();
 }
