d5e255ee71075ab64de488e2cbb82c4e2190ae4a
DopeyGreg <szulik.grzegorz@gmail.com>
2022-11-15 14:25:18 +0100

    dodane popularne

--- displayimages.php
+++ displayimages.php
@@ -4,6 +4,7 @@ require_once "connect.php";
 $kategoria = $_SESSION['kategoria'];
 
 $new_img = -4;//ile dni wstecz maja byc pokazywane zdjecia w tabeli nowe, teraz 4 dni wstecz
+$img_popularity = 5; //ilosc polubien zeby zdjecie stalo sie popularne
 
 if($kategoria === 'b' | $kategoria === 'wi' | $kategoria === 'j' )
   $zapytanie = sprintf('select img_filename, img_title, img_id from images where img_subcategory = "%s"', $kategoria);
@@ -13,6 +14,8 @@ elseif($kategoria == 'fav')
   $zapytanie = sprintf('select img_filename, img_title, img_id from images JOIN favourite ON img_id= images_img_id where users_user_id = "%s"', $_SESSION['user_id']);
 elseif($kategoria == 'new')
   $zapytanie = sprintf('select img_filename, img_title, img_id, img_uploaded from images where img_uploaded BETWEEN DATE_ADD(CURDATE(), INTERVAL "%d" DAY) AND CURDATE() ORDER BY img_uploaded DESC', $new_img);
+elseif($kategoria == 'pop')
+  $zapytanie = sprintf('select img_filename, img_title, img_id, COUNT(img_id) as ilosc_polubien from images JOIN favourite ON img_id= images_img_id GROUP BY img_id ORDER BY ilosc_polubien DESC');
 
 $polaczenie = @new mysqli($host, $db_user,$db_password,$db_name);
 // Include the database configuration file
@@ -32,30 +35,60 @@ else
     if ($query->num_rows > 0) 
     {
       while($row=$query->fetch_assoc())
-      {        
+      {
         $img_URL ='uploaded_images/'.$row["img_filename"];        
         $img_title = $row["img_title"];
         $img_id = $row["img_id"];
 
-        echo <<<END
-        <li>
-        <img src=$img_URL alt=""><h3>$img_title</h3>                      
-        END;
-        if(isset($_SESSION['zalogowany'])){
-          $user = $_SESSION['user_id'];
-          $zapytanie2 = sprintf('select * from favourite where images_img_id = "%s" AND users_user_id = "%s"', $img_id , $user);
-          $query2=$polaczenie->query($zapytanie2);   
-          $ff = $query2->num_rows;          
-          if ( $ff)        
-          $src = "icons/followed.png";
-        else 
-          $src = "icons/unfollowed.png";
-        echo <<<END
-        <a href="#" onclick="return false;"><img class="follow-icon" id=$img_id onclick="changeFollow(this)" src=$src></a> 
-        </li>
-        END;
-        } else 
-       echo "</li>";
+        if($kategoria == 'pop')
+        {
+          $ilosc_polubien = $row['ilosc_polubien'];
+          if($ilosc_polubien >= $img_popularity)
+          {
+            echo <<<END
+            <li>
+            <img src=$img_URL alt=""><h3>$img_title</h3>                      
+            END;
+            if(isset($_SESSION['zalogowany']))
+            {
+              $user = $_SESSION['user_id'];
+              $zapytanie2 = sprintf('select * from favourite where images_img_id = "%s" AND users_user_id = "%s"', $img_id , $user);
+              $query2=$polaczenie->query($zapytanie2);   
+              $ff = $query2->num_rows;          
+              if ( $ff)        
+                $src = "icons/followed.png";
+              else 
+                $src = "icons/unfollowed.png";
+            echo <<<END
+            <a href="#" onclick="return false;"><img class="follow-icon" id=$img_id onclick="changeFollow(this)" src=$src></a> 
+            </li>
+            END;
+            } else 
+            echo "</li>";
+          }
+        }
+        else
+        {
+          echo <<<END
+          <li>
+          <img src=$img_URL alt=""><h3>$img_title</h3>                      
+          END;
+          if(isset($_SESSION['zalogowany'])){
+            $user = $_SESSION['user_id'];
+            $zapytanie2 = sprintf('select * from favourite where images_img_id = "%s" AND users_user_id = "%s"', $img_id , $user);
+            $query2=$polaczenie->query($zapytanie2);   
+            $ff = $query2->num_rows;          
+            if ( $ff)        
+            $src = "icons/followed.png";
+          else 
+            $src = "icons/unfollowed.png";
+          echo <<<END
+          <a href="#" onclick="return false;"><img class="follow-icon" id=$img_id onclick="changeFollow(this)" src=$src></a> 
+          </li>
+          END;
+          } else 
+         echo "</li>";
+        }        
       }
     }
   }
