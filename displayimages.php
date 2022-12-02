91902fa14a0350af4514055798c085c41cd58ebc
Trater <szulik.grzegorz@gmail.com>
2022-12-02 22:02:37 +0100

    dsa

--- displayimages.php
+++ displayimages.php
@@ -3,7 +3,7 @@
 require_once "connect.php";
 $kategoria = $_SESSION['kategoria'];
 
-$new_img = -4;//ile dni wstecz maja byc pokazywane zdjecia w zakładce nowości, teraz 4 dni wstecz
+$new_img = -7;//ile dni wstecz maja byc pokazywane zdjecia w zakładce nowości, teraz 4 dni wstecz
 $img_popularity = 3; //ilosc polubien zeby zdjecie stalo sie popularne
 
 if($kategoria === 'b' | $kategoria === 'wi' | $kategoria === 'j' )
